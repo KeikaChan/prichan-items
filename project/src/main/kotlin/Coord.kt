fc05d03793e54cbd7b133ece2c02585a60599434
khrom <devs@outlook.jp>
2019-04-07 13:38:25 +0900

    [add] コーデ保存用にクラスを作った。 jsonとかで吐き出しやすくなる。

---
+++ project/src/main/kotlin/Coord.kt
@@ -0,0 +1,23 @@
+class Coord{
+    //名前はHTMLの要素名とpython実装時に合わせています。
+
+    var name :String =""
+    var image_url :String =""
+    var category :String =""
+    var item_id :String =""
+    var ticket_id :String =""
+    var color :String =""
+    var brand :String =""
+    var brand_image_url :String =""
+    var genre :String =""
+    var genre_image_url :String =""
+    var rarity :String =""
+    var like :String =""
+    var outfit_id :String =""
+    var outfit_image_url :String =""
+    var detail_url :String =""
+    var series_name :String =""
+    var series_url :String =""
+    var season :String ="" // あたしく追加。シーズン season1 (無印) / season 2 (ジュエル)
+    var note :String =""
+}
