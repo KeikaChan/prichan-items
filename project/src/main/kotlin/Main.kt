b9f3832b9f55d4f5829c0badeeb6aface7d25b45
devs@outlook.jp <devs@outlook.jp>
2020-01-05 00:15:22 +0900

    add calling for debug

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,8 +1,9 @@
 import resource.Season1Resource
+import seasons.Juel
 
 fun main() {
-    val one: Season = Season1Resource()
-    one.crawsCoords("https://prichan.jp/season1/items/")
-//    val juel: Season = Juel()
-//    juel.crawsCoords("https://prichan.jp/item/J01/")
+//    val one: Season = Season1Resource()
+//    one.crawsCoords("https://prichan.jp/season1/items/")
+    val juel: Season = Juel()
+    juel.crawsCoords("https://prichan.jp/item/J01/")
 }
